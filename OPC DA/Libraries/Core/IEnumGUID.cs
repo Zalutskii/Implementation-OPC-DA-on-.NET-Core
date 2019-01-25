42c3d453e8cb0557c651414403c54ac86d2ae2c3
Ivan <MozGangster@narod.ru>
2019-01-25 12:03:32 +1000

    Add OPC DA.sln

---
+++ OPC DA/Libraries/Core/IEnumGUID.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Core
+{
+	[Guid("0002E000-0000-0000-C000-000000000046"), ComImport, InterfaceType((short)1)]
+	interface IEnumGUID
+	{
+		[PreserveSig]
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		int Next(
+			[In] uint celt,
+			[Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] Guid[] rgelt,
+			[Out] out uint celtFetched);
+
+		[PreserveSig]
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		int Skip([In] uint celt);
+
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void Reset();
+
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void Clone([Out, MarshalAs(UnmanagedType.Interface)] out IEnumGUID @enum);
+	}
+}
