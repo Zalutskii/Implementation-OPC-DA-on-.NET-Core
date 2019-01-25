e368cea4c6d38691d51bc5db0e453e7182ee3db2
Ivan <MozGangster@narod.ru>
2019-01-25 12:23:31 +1000

    Add interfaces

---
+++ OPC DA/Libraries/Core/Interfaces/IOPCEnumGUID.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Core
+{
+	[Guid("55C382C8-21C7-4E88-96C1-BECFB1E3F483"), ComImport, InterfaceType((short)1)]
+	interface IOPCEnumGUID
+	{
+		[PreserveSig]
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		int Next(
+			[In] uint celt,
+			[Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 0)] Guid[] rgelt,
+			[Out] out uint celtFetched);
+
+		[PreserveSig]
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		int Skip([In] uint celt);
+
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void Reset();
+
+		[MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void Clone([Out, MarshalAs(UnmanagedType.Interface)] out IOPCEnumGUID @enum);
+	}
+
+}
